62513111846a43edbdd4d00747eea14bdd741cbe
bartush <vvictorix@gmail.com>
2023-06-29 14:50:20 +0300

    fixed info mode font issues

--- vv.emacs.windows10.el
+++ vv.emacs.windows10.el
@@ -83,7 +83,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "basis33" :foundry "outline" :slant normal :weight normal :height 180 :width normal))))
- '(Info-quoted ((t (:weight semi-bold :family "basis33"))))
+ '(variable-pitch ((default nil) (nil nil)))
+ '(fixed-pitch-serif ((t (:inherit default :foreground "light blue"))))
  '(tooltip ((default nil) (nil nil))))
 
 (set-fontset-font "fontset-default" 'windows-1251 "basis33") ;; set font for russian characters
